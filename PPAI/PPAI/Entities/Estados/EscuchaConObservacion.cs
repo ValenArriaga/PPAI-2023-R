d00802077b3228d05e7559192746dbf0da16f4a8
varriaga <varriaga@VARRIAGA>
2023-11-13 11:52:37 -0300

    clases concretas de estado - valentin

---
+++ PPAI/PPAI/Entities/Estados/EscuchaConObservacion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPAI.Entities.Estados
+{
+    class EscuchaConObservacion : EstadoA
+    {
+        public override EstadoA CrearProximoEstado(LlamadaEntity llamada)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
